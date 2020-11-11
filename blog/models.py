9fa638fdab482f3382ceedfc46c974ea22853433
Carlos Luna <carlos.lun.salazar@gmail.com>
2020-11-10 21:57:46 -0300

    tutorial terminado

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,9 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
@@ -33,3 +36,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+    
